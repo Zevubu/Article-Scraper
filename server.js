7f020ce818c8d482e165bff44f50b1f46beb5650
Zev Hoffman <zevubu@gmail.com>
2019-10-27 14:40:43 -0700

    set up my server

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const cheerio = require("cheerio");
 const axios = require("axios");
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// app will
 app.use(logger("dev"));
 
 app.use(express.urlencoded({extended: true}));
 
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// setting up 
+app.set('views', './views')
+app.engine(
+    "handlebars",
+    exphbs({
+        defaultLayout: "main"
+    })
+);
+app.set("view engine", "handlebars");
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, function (){
+    console.log("listening on port: " + PORT);
+});
