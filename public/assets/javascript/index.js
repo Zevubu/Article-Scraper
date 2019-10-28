569dae599ad91a94610d057425eecb7ae1f39a43
Zev Hoffman <zevubu@gmail.com>
2019-10-28 14:10:13 -0700

    finished writing out my index.js functions. started layingout my saved.js functions

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
         $.ajax({
             method:"PUT",
-            url:"/api/headlines/" + articleToSave. _id,
+            url:"/api/headlines/" + articleToSave._id,
             data: articleToSave
         })
         .then(function (data){
@@ -21,39 +21,66 @@ $(document).ready(function(){
         });
     };
 
+    function handleArticlescrape(){
+        $.get("/api/fetch")
+        .then(function (data){
+            // initPage()
+            console.log(`Scraped Data: ${data}`);
+            window.location.href="/";
+        });
+    };
+
+    function handleArticleClear(){
+        $.get("api/clear").then(function(data){
+            console.log(`Cleared Data: ${data}`);
+            articleContainer.empty();
+            location.reload();
+        });
+    };
+
+    
+
 
     $(document).on("click", ".btn.save", handleArticleSave);
     $(document).on("click", "scrape-new", handleArticlescrape);
     $(".clear").on("click", handleArticleClear);
 
     function createCard(article){
-        const card = $(`<section class="card">`).append(
-        )
+        let card = $(`<section class="card">`);
+        let cardHead = $(`<section class="card-header">`).append(
+            $('<h3>').append(
+                $(`<a class="article-title" target="_blank" rel="noopener noreferrer">`)
+                .text(article.title),
+                $(`<a class="article-link"  target="_blank" rel="noopener noreferrer">`)
+                .attr("href", article.link)
+                .text(article.link),
+                $("<a class='btn btn-success save'>Save Article</a>")
+
+            )
+        );
+        card.append(cardHead);
+        card.data("_id",article._id);
+        return card;
 
     }
-    // x
-    function createCard(article) {
-        var card = $("<div class='card'>");
-        var cardHead = $("<div class='card-header'>").append(
-        $("<h3>").append(
-        $("<a class='article-link; targer='_blank' rel='noopener noreferrer'>")
-        .attr("href", article.url)
-        .text(article.headline),
-        $("<a class='btn btn-success save'>Save Article</a>")
-        )
-    );
 
-
-
-    function renderArticles(articles){
-        const articleCard = [];
-
-        for(let i = 0; i< articles.length; i++){
-            articleCard.push(createCard(articles[i]));
-        }
-        articleContainer.append(articleCards);
+    function renderEmpty(){
+        let emptyAlert = 
+        $([`<section class="alert alert-warning text-center">`,
+        `<h4>The Data Base apears to be empty.</h4>`,
+        `</section>`,
+        `<section class="panel panel-default">`,
+        `<section class="panel-heading text-center">`,
+        `<h3> What would you like to do?</h3>`,
+        `</section>`,
+        `<section class="panel-body text-center">`,
+        `<h4><a class="scrape-new">Scrape New Content</a></h4>`,
+        `<h4><a href="/saved">View Saved Articles</a></h4>`,
+        `</section>`,
+        `</section>`
+    ].join(""));
+    articleContainer.append(emptyAlert);
     }
-
 
     function initPage(){
         $.get("/api/headlines?saved=false")
@@ -68,5 +95,13 @@ $(document).ready(function(){
         });
     }
 
+    function renderArticles(articles){
+        const articleCard = [];
+
+        for(let i = 0; i< articles.length; i++){
+            articleCard.push(createCard(articles[i]));
+        }
+        articleContainer.append(articleCards);
+    };
    
 })
---
+++ public/assets/javascript/saved.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    const articleContainer = $(".article-container");
+
+    function handleArticleDelete (){
+
+    };
+
+    function handleArticleNotes(){
+
+    };
+
+    function handleNoteSave(){
+
+    };
+
+    function handleNoteDelete(){
+
+    };
+
+    function handleArticleclear(){
+
+    };
+
+
+    $(document).on("click",".btn.delete", handleArticleDelete);
+    $(document).on("click", "btn.notes", handleArticleNotes);
+    $(document).on("click",".btn.delete", handleArticleDelete);
+    $(document).on("click", "btn.save", handleNoteSave);
+    $(document).on("click", "btn.note-delete", handleNoteDelete);
+    $(".clear").on("click", handleArticleclear);
+
+    function createCard(article){
+
+    };
+
+    function renderArticles(articles){
+
+    };
+    
+    function initPage(){
+
+    };
+
+})
