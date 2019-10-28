df7d8f1d9ba3991663a8b0088758601523d7c3f7
Zev Hoffman <zevubu@gmail.com>
2019-10-28 14:28:27 -0700

    built a base functions for handle Article delete & handle notes

--- public/assets/javascript/saved.js
+++ public/assets/javascript/saved.js
@@ -1,12 +1,71 @@
 $(document).ready(function(){
     const articleContainer = $(".article-container");
-
-    function handleArticleDelete (){
+   
+    function createCard(article){
 
     };
 
-    function handleArticleNotes(){
+    function renderArticles(articles){
 
+    };
+    
+    function renderEmpty(){
+
+    };
+
+    function renderNotesList(data){
+
+    };
+
+    function initPage(){
+
+    };
+
+    function handleArticleDelete (){
+        let articleToDelete = $(this).parents(".card").data();
+
+        $(this).parents(".card").remove();
+        console.log(articleToDelete._id);
+
+        $ajax({
+            method:"DELETE",
+            url:`/api/headlines/${articleToDelete._id}`
+        }).then(function(data){
+            if (data){
+                window.load = "/saved"
+            }
+        });
+    };
+
+    function handleArticleNotes(){
+        let currentArticle = $(this).parents(".card").data();
+
+        console.log(currentArticle);
+
+        $.get(`/api.notes/${currentArticle.id}`).then(function(data){
+            console.log(data);
+
+            let modalText =  $("<div class='container-fluid text-center'>").append(
+                $("<h4>").text("Notes For Article: " + currentArticle._id),
+                $("<hr>"),
+                $("<ul class='list-group note-container'>"),
+                $("<textarea placeholder='New Note' rows='4' cols='60'>"),
+                $("<button class='btn btn-success save'>Save Note</button>")
+              );
+              console.log(modalText);
+              bootbox.dialog({
+                  message: modalText,
+                  closeButton:true
+              });
+              let noteData = {
+                  _id: currentArticle._id,
+                  notes: data || []
+              };
+              console.log(`Notes Data: ${JSON.stringify(noteData)}`);
+
+              $(".btn.save").data("article", noteData);
+              renderNotesList(noteData);
+        });
     };
 
     function handleNoteSave(){
@@ -29,16 +88,5 @@ $(document).ready(function(){
     $(document).on("click", "btn.note-delete", handleNoteDelete);
     $(".clear").on("click", handleArticleclear);
 
-    function createCard(article){
 
-    };
-
-    function renderArticles(articles){
-
-    };
-    
-    function initPage(){
-
-    };
-
-})
+});
