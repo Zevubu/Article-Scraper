7ed20cac46a4ad222e002624cd9289f5be40e852
Zev Hoffman <zevubu@gmail.com>
2019-10-29 23:19:52 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const exphbs = require("express-handlebars");
+
 
 const PORT = process.env.PORT || 3000;
 
